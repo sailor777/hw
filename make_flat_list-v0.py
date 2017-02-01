c39a213de9641b1d24b80dd34239c56d79758e4e
sailor777 <pavlo.ok@gmail.com>
2017-02-01 14:38:12 +0200

    changes

--- make_flat_list-v0.py
+++ make_flat_list-v0.py
@@ -1,25 +1,33 @@
+#!/usr/bin/env python3.6
 
 # define
 #start_list = [1, [2, 3], ';', [[6, 7, [5], [[[6, 'a']]]]]]
 start_list = []
 flat_list = []
 str_tmp_list = ''
+t = ''
 
 #str_tmp_list = str(start_list)
 # or give a string from User
 str_tmp_list = str(input('Enter your list in string:'))
 
 str_tmp_list = str_tmp_list.replace('.',',')
-str_tmp_list = str_tmp_list.replace(',','')
+#str_tmp_list = str_tmp_list.replace(',','')
 str_tmp_list = str_tmp_list.replace('\'','')
 str_tmp_list = str_tmp_list.replace('[','')
 str_tmp_list = str_tmp_list.replace(']','')
 str_tmp_list = str_tmp_list.replace(' ','')
+str_tmp_list += ','
 
 start_list = list(str_tmp_list)
 
 for i in start_list:
-    try: flat_list.append(int(i))
-    except (ValueError): flat_list.append(i)
+    #print(i)
+    if i != ',':
+        t += i
+    else:
+        try: flat_list.append(int(t))
+        except (ValueError): flat_list.append(t)
+        t = ''
 
 print("Flat list is: %s" % flat_list)
