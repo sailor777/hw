d5ac70dcfda538a7ce3de709462c2b892e5713d3
Pavlo Komendant <sailor777@users.noreply.github.com>
2017-02-09 12:00:08 +0200

    Update func_factory.py

--- func_factory.py
+++ func_factory.py
@@ -7,10 +7,6 @@ def own_print(glob):
 func1 = own_print("foo")
 print(func1("bar"))
 
-func2 = own_print("One")
-print(func2("not Two"))
-print(func2("not Three"))
-
 func3 = own_print("Fafa")
 print(func3("Dodo"))
 print(func3("Mimimi"))
